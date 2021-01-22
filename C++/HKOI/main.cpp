179d1aa58103abc038fcbecc935a9f4a42c27bb1
William So <polyipseity@gmail.com>
2021-01-22 10:32:19 +0800

    Implement task selector
    Original commit: 885e5e52bef49ec60a9e9d01760b82fe584c25bb
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- C++/HKOI/main.cpp
+++ C++/HKOI/main.cpp
@@ -1,6 +1,31 @@
+#include <string>
+#include <iostream>
 #include "S191/unstoppable_onslaught.h"
 
-int main(int argc, char** argv) {
-    s191::main(argc, argv);
-    return 0;
+using namespace std;
+
+int main(int const argc, char *const *const argv) noexcept {
+    int result; // will eventually be set
+
+    bool choose;
+    do {
+        choose = false;
+        
+        string choice;
+        cout << "Choose a task: ";
+        getline(cin, choice);
+
+        // I heard you like giant if statements, no?
+        if (choice.empty()) {
+            cout << "No tasks selected." << endl;
+            result = 0;
+        } else if (choice == "s191")
+            result = s191::main(argc, argv);
+        else {
+            cerr << "Task not found: " << choice << endl;
+            choose = true;
+        }
+    } while (choose);
+
+    return result;
 }
