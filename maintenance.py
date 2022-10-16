75a32d0e955ede8d6ebb04818051b226a673e147
William So <polyipseity@gmail.com>
2022-10-16 21:06:45 +0800

    Improve 'maintenance.py' so that uncaught exceptions are handled
    Original commit ID: fd87a7a684bd26d81adae1332e90eb515e8f7c55
    
    Original commit ID: 38e8f8c5686b657525943d3f85f3d4919ff3f647
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- maintenance.py
+++ maintenance.py
@@ -1,30 +1,35 @@
+import logging as _logging
 import os as _os
 import pathlib as _pathlib
 import types as _types
 import typing as _typing
 import inspect as _inspect
 
-import tools as _tools
-
 
 def main() -> None:
-    frame: _types.FrameType | None = _inspect.currentframe()
-    if not frame:
-        raise ValueError(frame)
-    filename: str = _inspect.getframeinfo(frame).filename
-    folder: _pathlib.PurePath = _pathlib.PurePath(filename).parent
+    try:
+        frame: _types.FrameType | None = _inspect.currentframe()
+        if not frame:
+            raise ValueError(frame)
+        filename: str = _inspect.getframeinfo(frame).filename
+        folder: _pathlib.PurePath = _pathlib.PurePath(filename).parent
+        import tools
 
-    def generate_args0() -> _typing.Iterator[str]:
-        root: str
-        files: _typing.Sequence[str]
-        for root, _, files in _os.walk(folder):
-            file: str
-            for file in files:
-                if file.endswith('.md'):
-                    yield _os.path.join(root, file)
-    generate_args: _typing.Sequence[str] = tuple(generate_args0())
-    print(f'Generating notes from {len(generate_args)} file(s)')
-    _tools.generate.main.main(generate_args)
+        def generate_args0() -> _typing.Iterator[str]:
+            root: str
+            files: _typing.Sequence[str]
+            for root, _, files in _os.walk(folder):
+                file: str
+                for file in files:
+                    if file.endswith('.md'):
+                        yield _os.path.join(root, file)
+        generate_args: _typing.Sequence[str] = tuple(generate_args0())
+        print(f'Generating notes from {len(generate_args)} file(s)')
+        tools.generate.main.main(generate_args)
+    except:
+        _logging.exception('Uncaught exception')
+    finally:
+        input('Press <enter> to exit')
 
 
 if __name__ == '__main__':
