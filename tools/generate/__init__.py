5057c7de52e46711ffff0a16e332ce0cfa5476f1
William So <polyipseity@gmail.com>
2022-10-19 20:51:10 +0800

    Make 'tools' into its own submodule
    Original commit ID: a77747abaf1b4527d16bd990b9dff8a4e38fef03
    
    Original commit ID: c3c28a83b0bd8156e2ad00bd23f3cc819cd32123
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- tools/generate/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import main as main
-from . import environment as environment
-from .read import *
-from .venv import *
-from .write import *
--- tools/version.py
+++
@@ -1,5 +0,0 @@
-import distutils.version as _distutils_version
-
-version: _distutils_version.Version = _distutils_version.StrictVersion(
-    '1.0.0'
-)
