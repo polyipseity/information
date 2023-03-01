3c8da692d6f425fcb3385897be27a49ced144719
William So <polyipseity@gmail.com>
2023-03-01 12:05:31 +0800

    Move `tools/` into `tools/pytextgen/`
    Signed-off-by: William So <polyipseity@gmail.com>

--- init.py
+++ init.py
@@ -180,9 +180,9 @@ def main(args: Arguments) -> _typing.NoReturn:
             success: bool = True
             if inputs:
                 try:
-                    import tools.main
+                    import tools.pytextgen.main as main
 
-                    entry: _argparse.Namespace = tools.main.parser().parse_args(
+                    entry: _argparse.Namespace = main.parser().parse_args(
                         tuple(
                             _itertools.chain(
                                 args.arguments,
