8c278bb02fb77106f522919ec81970d6db01ed73
William So <polyipseity@gmail.com>
2023-09-14 01:04:45 +0800

    Improve `test.ps1` slightly
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 1ab7be5eaab56c483744e991259773a68465780b

--- self/academia/computer science/HKUST COMP 2011/lab 1/test.ps1
+++ self/academia/computer science/HKUST COMP 2011/lab 1/test.ps1
@@ -1,4 +1,4 @@
-$program = "./lab1.exe"
+$program = './lab1.exe'
 
 for ($i = 1;
 	-not ((Test-Path "./testcase/input${i}.txt", "./testcase/output${i}.txt" -PathType Leaf) -contains $false);
@@ -11,6 +11,6 @@ for ($i = 1;
 		$header = "=======Test ${i} failed======="
 		Write-Output $header
 		Write-Output $output
-		Write-Output ("=" * $header.Length)
+		Write-Output ('=' * $header.Length)
 	}
 }
