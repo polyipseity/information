0da2a6e8136a39724ec73a8a9747a91ac9d45acd
William So <polyipseity@gmail.com>
2023-09-14 00:58:56 +0800

    Improve `test.ps1` to add automated testing
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 239e102a5aaf9415e4cf15db5769209795d37833

--- self/academia/computer science/HKUST COMP 2011/lab 1/test.ps1
+++ self/academia/computer science/HKUST COMP 2011/lab 1/test.ps1
@@ -1,5 +1,15 @@
 $program = "./lab1.exe"
 
-for ($i = 1; Test-Path "./testcase/input${i}.txt"; ++$i) {
-	Get-Content "./testcase/input${i}.txt" | & $program > "./testcase/output${i}.txt"
+for ($i = 1;
+	-not ((Test-Path "./testcase/input${i}.txt", "./testcase/output${i}.txt" -PathType Leaf) -contains $false);
+	++$i) {
+	$output = Get-Content "./testcase/input${i}.txt" | & $program
+	if ($output -eq (Get-Content "./testcase/output${i}.txt")) {
+		Write-Output "Test ${i} succeeded"
+	}
+	else {
+		Write-Output "=======Test ${i} failed======="
+		Write-Output $output
+		Write-Output '=============================='
+	}
 }
