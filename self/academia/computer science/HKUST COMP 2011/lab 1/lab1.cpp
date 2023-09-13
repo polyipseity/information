a026df6f764eafe247919d5bd4bf40bb4178848f
William So <polyipseity@gmail.com>
2023-09-13 10:34:59 +0800

    Initialize COMP 2011 lab 1
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 9545ce587e01c2fb5c1680ba2c646eadcb03a213

---
+++ self/academia/computer science/HKUST COMP 2011/lab 1/lab1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int width, height; // Define 2 variables that will hold the width and height respectively
+
+    cout << "Please input the width of the rectangle: "; 
+    cin >> width; 
+    cout << "Please input the height of the rectangle: "; 
+    cin >> height; 
+
+    // TODO: complete your program here
+    
+    return 0;
+}
