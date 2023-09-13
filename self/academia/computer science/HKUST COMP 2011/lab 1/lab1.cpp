4cdbe1e45dad81661801aef25aab635b848d7c9e
William So <polyipseity@gmail.com>
2023-09-13 10:45:59 +0800

    Complete `lab1.cpp`
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 2e2a4958a043f511a142ff4cad1214f661249ed3

--- self/academia/computer science/HKUST COMP 2011/lab 1/lab1.cpp
+++ self/academia/computer science/HKUST COMP 2011/lab 1/lab1.cpp
@@ -9,5 +9,15 @@ int main()
 	std::cout << "Please input the height of the rectangle: ";
 	std::cin >> height;
 
+	if (width <= 0 || height <= 0)
+	{
+		std::cout << "Error: the width and height must be positive!";
+	}
+	else
+	{
+		std::cout << "The perimeter of the rectangle is " << (width + height) * 2;
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
