f1fe989dd70e224a7f859c2171569384ee792eab
William So <polyipseity@gmail.com>
2023-09-20 11:33:56 +0800

    Prepare lab 2
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: ca7f571cdb6ae21cf174c28d56926e2dd7864541

---
+++ self/academia/computer science/HKUST COMP 2011/lab 2/lab2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1, number2; // Both inputs will be positive integers, but you do not know which is larger
+    int small, large; // small should be the smaller of the two numbers, large should be the larger
+    int finalScore = 0; // The final score of the game
+
+    // Get the range from the user
+    cin >> number1 >> number2;
+
+    /* Please do not change the code above this line */ 
+
+    // Task 1 - Find the small and large bounds of the range
+    ///////////////////
+    // Your Code Here //
+    ///////////////////
+
+    // Task 2 - Find the perfect numbers in the range and update the final score
+    ///////////////////
+    // Your Code Here //
+    ///////////////////
+
+
+    /* Please do not change the code below this line */
+
+    cout << finalScore;
+    return 0;
+}
