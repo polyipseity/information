f5e863b39d353e90f544153441cb4c1a3f1267eb
William So <polyipseity@gmail.com>
2023-09-26 21:55:26 +0800

    Complete 6 classworks
    Original commit: 9de9d3111a7ede2a534aee85d13b709be4b1ffed
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/classwork 4/main.c
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/classwork 4/main.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 
-void display_matrix(int matrix[][2], int num_row) {
-  for (int r = 0; r < num_row; r++) {
-    for (int c = 0; c < 2; c++) {
+void display_matrix(int matrix[][2], int num_row)
+{
+  for (int r = 0; r < num_row; r++)
+  {
+    for (int c = 0; c < 2; c++)
+    {
       printf("\t%d", matrix[r][c]);
     }
     printf("\n");
   }
 }
 
-int main() {
+int main()
+{
   int matrix_A[3][2] = {
-    {0, 1},
-    {2, 3},
-    {4, 5}
-  };
-  
+      {0, 1},
+      {2, 3},
+      {4, 5}};
+
   int matrix_B[3][2] = {
-    {0, 1},
-    {2, 3},
-    {4, 5}
-  };
-  
+      {0, 1},
+      {2, 3},
+      {4, 5}};
+
   int result[3][2] = {0};
-  
+
   // your code starts here
-  
+  for (int ii = 0; ii < 3; ++ii)
+  {
+    for (int jj = 0; jj < 2; ++jj)
+    {
+      result[ii][jj] = matrix_A[ii][jj] + matrix_B[ii][jj];
+    }
+  }
   // your code ends here
-  
+
   printf("A = \n");
   display_matrix(matrix_A, 3);
   printf("\nB = \n");
--- self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/classwork 5/main.c
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/classwork 5/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-// #include <string.h> // uncomment this line if needed 
+// #include <string.h> // uncomment this line if needed
 
-int main() {
+int main()
+{
   char input[] = "Welcome to HKUST Robotics Team Software Tutorial";
   printf("Before trimming: %s\n", input);
 
   // your code starts here
-  
+  int len = 0;
+  for (; len < sizeof(input) / sizeof(char); ++len)
+  {
+    if (input[len] == ' ')
+    {
+      break;
+    }
+  }
   // your code ends here
-  
-  printf("After trimming: %s\n", input);
+
+  printf("After trimming: %.*s\n", len, input);
   return 0;
 }
