a5360d6df6e8fc68d72bc273beaaf58f732589d0
William So <polyipseity@gmail.com>
2023-09-27 17:43:22 +0800

    Work on homework 1
    Original commit: 75489716033abd1996de6c440efb64a458211653
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 1/main.c
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 1/main.c
@@ -1,16 +1,69 @@
-// Please don't include any extra libraries in your homework. We already included all necessary libraries in the skeleton code.
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "ast_t.h"
+#include "l_stack_t.h"
+#include "str_t.h"
 
-const char* const msg_cannot_input = "Please input a math equation:\n";
-const char* const msg_cannot_output = "Output:\n";
-const char* const error_msg = "ERROR! The string input is not supported!!\n";
+char const *const msg_cannot_input = "Please input a math equation:\n";
+char const *const msg_cannot_output = "Output:\n";
+char const *const error_msg = "ERROR! The string input is not supported!!\n";
 
-int main(){
-    
-    return 0;
+void read_line(str_t *ret, FILE *stream)
+{
+    for (char cc = getc(stream); cc != EOF && cc != '\n'; cc = getc(stream))
+    {
+        size_t idx = ret->len;
+        ret = str_t_realloc(ret, idx + 1);
+        ret->data[idx] = cc;
+    }
 }
 
-// Deleted code is debugged code.
+bool parse(ast_t **ret, str_t *input)
+{
+    *ret = NULL;
+    intmax_t val = 0;
+    for (size_t ii = 0; ii < input->len; ++ii)
+    {
+        char chr = ret->data[ii];
+        switch (chr)
+        {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            val *= 10;
+            val += chr - '0';
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            break;
+        case '(':
+            break;
+        case ')':
+            break;
+        default:
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(void)
+{
+    str_t *input = str_t_malloc(0);
+    read_line(input, stdin);
+    str_t *ast_t = str_t_malloc(0);
+    parse(ast_t, input);
+    str_t_free(ast_t);
+    str_t_free(input);
+    return 0;
+}
