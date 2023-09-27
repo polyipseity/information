f698e4ce0e3c552ea0553462e826e1d277c8288a
William So <polyipseity@gmail.com>
2023-09-28 07:27:52 +0800

    Reformat homework 2 `main.c`
    Original commit: 9bbc939af685908788390da0ae65ff4c833608a3
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 2/main.c
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 2/main.c
@@ -7,38 +7,47 @@
 #define MAZE_ROW_NUM 10
 #define MAZE_COL_NUM 20
 
-
 int TREASURE_ROW;
 int TREASURE_COL;
 //              0     1    2      3
-enum direction {UP, DOWN, LEFT, RIGHT};
+enum direction
+{
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT
+};
 enum direction EXIT_DIRECTION;
 // refer to Advanced C about enums
 
-const char* const msg_prompt_input_movement = "Enter a direction to move (w: up, a: left, s: down, d: right): \n";
-const char* const msg_prompt_input_treasure_location = "Please enter the row and column of the treasure\n";
-const char* const msg_prompt_input_exit_direction = "Please enter the exit direction (0: up, 1: down, 2: left, 3: right)\n";
-const char* const msg_congratulation = "Congratulations! You found the treasure!\n";
-const char* const msg_cannot_move_left = "You cannot move left!\n";
-const char* const msg_cannot_move_right = "You cannot move right!\n";
-const char* const msg_cannot_move_up = "You cannot move up!\n";
-const char* const msg_cannot_move_down = "You cannot move down!\n";
-
-
+const char *const msg_prompt_input_movement = "Enter a direction to move (w: up, a: left, s: down, d: right): \n";
+const char *const msg_prompt_input_treasure_location = "Please enter the row and column of the treasure\n";
+const char *const msg_prompt_input_exit_direction = "Please enter the exit direction (0: up, 1: down, 2: left, 3: right)\n";
+const char *const msg_congratulation = "Congratulations! You found the treasure!\n";
+const char *const msg_cannot_move_left = "You cannot move left!\n";
+const char *const msg_cannot_move_right = "You cannot move right!\n";
+const char *const msg_cannot_move_up = "You cannot move up!\n";
+const char *const msg_cannot_move_down = "You cannot move down!\n";
 
 char MAZE[MAZE_ROW_NUM][MAZE_COL_NUM] = {};
 
-void maze_init(){
-  for(int i = 0; i < MAZE_ROW_NUM; i++){
-    for(int j =0; j < MAZE_COL_NUM; j++){
+void maze_init()
+{
+  for (int i = 0; i < MAZE_ROW_NUM; i++)
+  {
+    for (int j = 0; j < MAZE_COL_NUM; j++)
+    {
       MAZE[i][j] = '-';
     }
   }
 }
 
-void printMaze(){
-  for(int i = 0; i < MAZE_ROW_NUM; i++){
-    for(int j =0; j < MAZE_COL_NUM; j++){
+void printMaze()
+{
+  for (int i = 0; i < MAZE_ROW_NUM; i++)
+  {
+    for (int j = 0; j < MAZE_COL_NUM; j++)
+    {
       printf("%c ", MAZE[i][j]);
     }
     printf("\n");
@@ -46,28 +55,31 @@ void printMaze(){
   printf("\n");
 }
 
-void printVariables(){
+void printVariables()
+{
   // for your debugging purposes only
   // your final submission should not call this function
-  switch(EXIT_DIRECTION){
-    case UP:
-      printf("The exit is at the top of the treasure.\n");
-      break;
-    case DOWN:
-      printf("The exit is at the bottom of the treasure.\n");
-      break;
-    case LEFT:
-      printf("The exit is at the left of the treasure.\n");
-      break;
-    case RIGHT:
-      printf("The exit is at the right of the treasure.\n");
-      break;
+  switch (EXIT_DIRECTION)
+  {
+  case UP:
+    printf("The exit is at the top of the treasure.\n");
+    break;
+  case DOWN:
+    printf("The exit is at the bottom of the treasure.\n");
+    break;
+  case LEFT:
+    printf("The exit is at the left of the treasure.\n");
+    break;
+  case RIGHT:
+    printf("The exit is at the right of the treasure.\n");
+    break;
   }
   printf("TREASURE_ROW: %d\n", TREASURE_ROW);
   printf("TREASURE_COL: %d\n", TREASURE_COL);
 }
 
-int main(){
+int main()
+{
   // input TREASURE ROW, TREASURE_COL and EXIT_DIRECTION
   printf("%s", msg_prompt_input_treasure_location);
   scanf("%d", &TREASURE_ROW);
