2a430f79bede5d20158986901e42b22bc5ed2589
William So <polyipseity@gmail.com>
2023-09-27 10:42:26 +0800

    Add COMP 2011 lab 3
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 24ff8819caaade42b7dcded2b98c8e94d6be1e8c

---
+++ self/academia/computer science/HKUST COMP 2011/lab 3/test.ps1
@@ -0,0 +1,16 @@
+$program = './lab3.exe'
+
+for ($i = 1;
+	-not ((Test-Path "./testcase/input${i}.txt", "./testcase/output${i}.txt" -PathType Leaf) -contains $false);
+	++$i) {
+	$output = Get-Content "./testcase/input${i}.txt" | & $program
+	if ($output -eq (Get-Content "./testcase/output${i}.txt")) {
+		Write-Output "Test ${i} succeeded"
+	}
+	else {
+		$header = "=======Test ${i} failed======="
+		Write-Output $header
+		Write-Output $output
+		Write-Output ('=' * $header.Length)
+	}
+}
