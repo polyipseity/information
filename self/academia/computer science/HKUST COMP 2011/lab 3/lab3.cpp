a09a482b06060c9f91400da3aebbd306414fd33a
William So <polyipseity@gmail.com>
2023-09-27 11:03:56 +0800

    Complete COMP 2011 lab 3 task 4
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 772bdca0fda715643f7d2ade96d7567830558081

--- self/academia/computer science/HKUST COMP 2011/lab 3/lab3.cpp
+++ self/academia/computer science/HKUST COMP 2011/lab 3/lab3.cpp
@@ -102,7 +102,7 @@ int main()
     // TODO: fill this part
     // Read the option, the 4-letter text and the key from fin
     // Close the file after reading
-
+    cin >> option >> c1 >> c2 >> c3 >> c4 >> key;
     // End of TODO
 
     // Perform encryption or decryption
