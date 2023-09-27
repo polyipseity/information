826832ed846eee65f9cb8850debef10fe54dd841
William So <polyipseity@gmail.com>
2023-09-27 14:03:43 +0800

    Setup 3 homework
    Original commit: ee9c2ee1f3c7d7558613fafe5b95a6ecfb33ca45
    
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 1/main.c
@@ -0,0 +1,16 @@
+// Please don't include any extra libraries in your homework. We already included all necessary libraries in the skeleton code.
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+const char* const msg_cannot_input = "Please input a math equation:\n";
+const char* const msg_cannot_output = "Output:\n";
+const char* const error_msg = "ERROR! The string input is not supported!!\n";
+
+int main(){
+    
+    return 0;
+}
+
+// Deleted code is debugged code.
