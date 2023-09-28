5fd1c6f93dd651f8ecd3822c5d99a8aeabfdaa59
William So <polyipseity@gmail.com>
2023-09-28 17:32:32 +0800

    Revert "Try approximating `ln`, `exp`, and `pow`"
    Giving up...
    
    This reverts commit da584dd70fb0fbc119e9bfceca1267cc800dc56f.
    
    Original commit: ca5f2259bd3169d8247daf9cc4af8bb88584ba77
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 1/ast.c
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 1/ast.c
@@ -1,5 +1,5 @@
 #include <limits.h>
-#include <stdbool.h>
+#include <math.h>
 #include "ast.h"
 
 op_t op_from_char(char chr)
@@ -39,81 +39,9 @@ double divide(double a, double b)
 {
 	return a / b;
 }
-double exp(double a)
-{
-	double ret = 0;
-	for (int ii = 0; ii < 1024; ++ii)
-	{
-		double term = 1;
-		for (int jj = 1; jj <= ii; ++jj)
-		{
-			term *= a;
-			term /= jj;
-		}
-		ret += term;
-	}
-	return ret;
-}
-double ln(double a)
-{
-	if (a == 0)
-	{
-		return 0 / 0;
-	}
-	bool negative = a < 0;
-	if (negative)
-	{
-		a = -a;
-	}
-	int n = 0;
-	while (a >= 10)
-	{
-		++n;
-		a /= 10;
-	}
-	while (a < 1)
-	{
-		--n;
-		a *= 10;
-	}
-	if (negative)
-	{
-		a = -a;
-	}
-	int y = (a - 1) / (a + 1);
-	double ret = 0;
-	for (int ii = 0; ii < 10; ++ii)
-	{
-		double term = 2.0 / (2 * ii + 1);
-		for (int jj = 0; jj < 2 * ii + 1; ++jj)
-		{
-			term *= y;
-		}
-		ret += term;
-	}
-	return ret + 2.302585092994045684 * (n - 1);
-}
 double power(double a, double b)
 {
-	// f(x) = a^x
-	// f(64) a^64
-	// f'(64) = a^64 + 64a^63/1!(b-64) + 64*63*a^62/2!1(b-64)^2
-	long long exp = b;
-	double coefficient = 1;
-	for (int ii = 0; ii < exp; ++ii)
-	{
-		coefficient *= a;
-	}
-	double term = 1, ret = coefficient * term;
-	for (int terms = 1; terms <= exp; ++terms)
-	{
-		coefficient /= terms;
-		coefficient /= a;
-		coefficient *= exp - terms + 1;
-		term *= b - exp;
-		ret += coefficient * term;
-	}
-	return ret;
+	return pow(a, b);
 }
 int const op_priorities[] = {0, 0, 1, 1, 2, -1, INT_MIN};
 op_function *const op_functions[] = {&plus, &minus, &multiply, &divide, &power};
