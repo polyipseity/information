9e445095a236c9cb91ef93b12ae7b05233e89c7e
William So <polyipseity@gmail.com>
2023-09-28 09:39:39 +0800

    Complete homework 3 `main.c`
    Original commit: 4ba7bca38b4da9e0b713f7f8eb2fe80ccd9c9a78
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 3/main.c
+++ self/academia/engineering/HKUST Robotics Team/SW 2023/tutorial 1/homework 3/main.c
@@ -1,4 +1,5 @@
 // Please don't include any extra libraries in your homework. We already included all necessary libraries in the skeleton code.
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -12,11 +13,47 @@ char const *const msg_prompt_number = "Please enter a set of number:\n";
 char const *const msg_prompt_current_number_system = "Please enter the current number system:\n";
 char const *const msg_prompt_number_system_to_convert = "Please enter the number system you want the set of number be converted to:\n";
 char const *const msg_output = "Output=";
+int_fast8_t const digits_c2i[] = {
+    ['0'] = 0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    ['a'] = 10,
+    11,
+    ['A'] = 10,
+    11,
+};
+char const digits_i2c[] = {
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+};
 
 int main()
 {
-
-    printf("%s", msg_prompt_number);
+    char const *error_msgs_system[] = {
+        [3] = error_msg_trinary,
+        [10] = error_msg_decimal,
+        [12] = error_msg_duodecimal,
+    };
+    char input[42];
+    printf_s("%s", msg_prompt_number);
+    scanf_s("%s", &input, sizeof input / sizeof(char));
 
     /**
      * @brief
@@ -24,8 +61,26 @@ int main()
      */
 
     // TODO:
-
-    printf("%s", msg_prompt_current_number_system);
+    int_fast8_t base;
+    printf_s("%s", msg_prompt_current_number_system);
+    scanf_s("%" SCNdFAST8, &base);
+    if (base != 3 && base != 10 && base != 12)
+    {
+        printf_s("%s", error_msg_unsupported_system);
+        return 1;
+    }
+    uint_fast64_t val = 0;
+    for (int ii = 0; input[ii] != '\0'; ++ii)
+    {
+        int_fast8_t digit = input[ii] < sizeof digits_c2i / sizeof(int_fast8_t) ? digits_c2i[input[ii]] : 0;
+        if (digit >= base || digit == 0 && input[ii] != '0')
+        {
+            printf_s("%s", error_msgs_system[base]);
+            return 1;
+        }
+        val *= base;
+        val += digit;
+    }
 
     /**
      * @brief
@@ -33,7 +88,14 @@ int main()
      */
 
     // TODO:
-    printf("%s", msg_prompt_number_system_to_convert);
+    int_fast8_t new_base;
+    printf_s("%s", msg_prompt_number_system_to_convert);
+    scanf_s("%" SCNdFAST8, &new_base);
+    if (new_base != 3 && new_base != 10 && new_base != 12)
+    {
+        printf_s("%s", error_msg_unsupported_system);
+        return 1;
+    }
 
     /**
      * @brief
@@ -44,6 +106,21 @@ int main()
      */
 
     // TODO:
+    printf_s("%s", msg_output);
+    if (val == 0)
+    {
+        printf_s("0\n");
+        return 0;
+    }
+    char output[41];
+    int_fast8_t len = 0;
+    while (val != 0)
+    {
+        ++len;
+        output[sizeof output / sizeof(char) - len] = digits_i2c[val % new_base];
+        val = val / new_base;
+    }
+    printf_s("%.*s\n", len, &output[sizeof output / sizeof(char) - len]);
 
     return 0;
 }
