28c921ea084b0564925115fd88b2a994651eafe0
William So <polyipseity@gmail.com>
2023-10-05 22:38:03 +0800

    Revert "Format `lab4.cpp`"
    This reverts commit 3dfb38d90c9d678c573e57cb30bb92918a570fc9.
    
    Signed-off-by: William So <polyipseity@gmail.com>
    
    # Conflicts:
    #	self/academia/computer science/HKUST COMP 2011/lab 4/lab4.cpp
    
    Private-commit: 42a7cf877cf564bede577a62fadb087f4221c2bc

--- self/academia/computer science/HKUST COMP 2011/lab 4/lab4.cpp
+++ self/academia/computer science/HKUST COMP 2011/lab 4/lab4.cpp
@@ -5,8 +5,7 @@ using namespace std;
 const int MAX_RADAR_CAPACITY = 5;
 
 // Task 1: Initialize the radar system
-void initialize_radar_system(int radar[])
-{
+void initialize_radar_system(int radar[]) {
     // Initialize all radar elements to 0 (no alien detected).
     ///////////////////
     // Your Code Here //
@@ -19,8 +18,7 @@ void initialize_radar_system(int radar[])
 
 // Task 2: Detect and track an incoming alien ship
 // tips: if radar is marked as 0, it means no alien ship is detected, so we can track it.
-bool detect_alien_ship(int radar[], int alienID)
-{
+bool detect_alien_ship(int radar[], int alienID) {
     // Check if radar is already full
     ///////////////////
     // Your Code Here //
@@ -38,8 +36,7 @@ bool detect_alien_ship(int radar[], int alienID)
 
 // Task 3: Engage in battle and eliminate all alien ships with a specific ID
 // tips: set eliminated alien ships to 0, no need to remove them from the array.
-int engage_in_battle(int radar[], int alienID)
-{
+int engage_in_battle(int radar[], int alienID) {
     ///////////////////
     // Your Code Here //
     ///////////////////
@@ -56,8 +53,7 @@ int engage_in_battle(int radar[], int alienID)
 }
 
 // Task 4: Count the total number of alien ships currently in radar
-int count_total_alien_ships(int radar[])
-{
+int count_total_alien_ships(int radar[]) {
     ///////////////////
     // Your Code Here //
     ///////////////////
@@ -75,8 +71,7 @@ int count_total_alien_ships(int radar[])
 // Task 5: Display the current radar status
 // tips: you need to display the radar status in the following format:
 //      Radar Status: [1 1 1 0 0]
-void display_radar_status(int radar[])
-{
+void display_radar_status(int radar[]) {
     ///////////////////
     // Your Code Here //
     ///////////////////
@@ -92,8 +87,7 @@ void display_radar_status(int radar[])
     cout << ']' << endl;
 }
 
-int main()
-{
+int main() {
     int radar[MAX_RADAR_CAPACITY];
     initialize_radar_system(radar);
     cout << "Alien Invasion Game Menu:" << endl;
@@ -103,50 +97,46 @@ int main()
     cout << "4. Display radar status" << endl;
     cout << "0. Quit the game" << endl;
     int choice;
-    do
-    {
+    do {
         cout << "Enter your choice: ";
         cin >> choice;
 
-        int alienID;         // Declare alienID outside of the switch statement
+        int alienID; // Declare alienID outside of the switch statement
         int eliminatedCount; // Declare eliminatedCount outside of the switch statement
         int totalAlienShips; // Declare totalAlienShips outside of the switch statement
 
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter the ID of the alien ship to detect: ";
-            cin >> alienID;
-            if (detect_alien_ship(radar, alienID))
-            {
-                cout << "Alien ship with ID " << alienID << " detected and tracked." << endl;
-            }
-            else
-            {
-                cout << "Radar is already full. Cannot track more aliens." << endl;
-            }
-            break;
-        case 2:
-            cout << "Enter the ID of the alien ship to engage in battle: ";
-            cin >> alienID;
-            eliminatedCount = engage_in_battle(radar, alienID);
-            cout << "Eliminated " << eliminatedCount << " alien ship(s) with ID " << alienID << "." << endl;
-            break;
-        case 3:
-            totalAlienShips = count_total_alien_ships(radar);
-            cout << "Total number of detected alien ships: " << totalAlienShips << endl;
-            break;
-        case 4:
-            display_radar_status(radar);
-            break;
-        case 0:
-            cout << "Exiting the game. Goodbye!";
-            break;
-        default:
-            cout << "Invalid choice. Please try again." << endl;
+        switch (choice) {
+            case 1:
+                cout << "Enter the ID of the alien ship to detect: ";
+                cin >> alienID;
+                if (detect_alien_ship(radar, alienID)) {
+                    cout << "Alien ship with ID " << alienID << " detected and tracked." << endl;
+                } else {
+                    cout << "Radar is already full. Cannot track more aliens." << endl;
+                }
+                break;
+            case 2:
+                cout << "Enter the ID of the alien ship to engage in battle: ";
+                cin >> alienID;
+                eliminatedCount = engage_in_battle(radar, alienID);
+                cout << "Eliminated " << eliminatedCount << " alien ship(s) with ID " << alienID << "." << endl;
+                break;
+            case 3:
+                totalAlienShips = count_total_alien_ships(radar);
+                cout << "Total number of detected alien ships: " << totalAlienShips << endl;
+                break;
+            case 4:
+                display_radar_status(radar);
+                break;
+            case 0:
+                cout << "Exiting the game. Goodbye!";
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
         }
 
     } while (choice != 0);
 
     return 0;
 }
+
