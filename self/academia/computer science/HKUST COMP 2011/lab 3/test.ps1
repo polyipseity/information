1602661a26393d59cc97e30002c73429f1b57062
William So <polyipseity@gmail.com>
2023-10-07 12:27:23 +0800

    Fix `test.ps1` for output lines with leading or trailing whitespaces
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 61474e4e4ee357401cdfdf8a42b9f64ac1a95a6c

--- self/academia/computer science/HKUST COMP 2011/lab 3/test.ps1
+++ self/academia/computer science/HKUST COMP 2011/lab 3/test.ps1
@@ -3,7 +3,7 @@ $program = './lab3.exe'
 for ($i = 1;
 	-not ((Test-Path "./testcase/input${i}.txt", "./testcase/output${i}.txt" -PathType Leaf) -ccontains $false);
 	++$i) {
-	$output = (Get-Content "./testcase/input${i}.txt" | & $program).Trim() -split "`r`n|[`n`v`f`r`u{85}`u{2028}`u{2029}]" -join "`n"
+	$output = ((Get-Content "./testcase/input${i}.txt" | & $program) -join "`n").Trim() -split "`r`n|[`n`v`f`r`u{85}`u{2028}`u{2029}]" -join "`n"
 	if ($output -ceq ((Get-Content "./testcase/output${i}.txt" -Raw).Trim() -split "`r`n|[`n`v`f`r`u{85}`u{2028}`u{2029}]" -join "`n")) {
 		Write-Output "Test ${i} succeeded"
 	}
