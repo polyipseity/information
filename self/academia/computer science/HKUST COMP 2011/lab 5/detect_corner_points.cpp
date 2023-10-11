cd7211c2dd3f5d9de8b393c189aaa67041a1bab7
William So <polyipseity@gmail.com>
2023-10-11 17:48:11 +0800

    Complete COMP 2011 lab 5
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: d4f57f30c770c089ec2f9b3bfaf683799b0b597d

--- self/academia/computer science/HKUST COMP 2011/lab 5/detect_corner_points.cpp
+++ self/academia/computer science/HKUST COMP 2011/lab 5/detect_corner_points.cpp
@@ -9,6 +9,35 @@
 // corner_points[3]: column index of the bottom right corner
 void detect_corner(const int arr[][MAX_DIMENSION], int row, int col, int corner_points[]) {
     /***************Start your code here***************/
-
+    [arr, col, corner_points, row]()
+    {
+        for (int row_cur{}; row_cur < row; ++row_cur)
+        {
+            for (int col_cur{}; col_cur < col; ++col_cur)
+            {
+                if (arr[row_cur][col_cur])
+                {
+                    corner_points[0] = row_cur;
+                    corner_points[1] = col_cur;
+                    return;
+                }
+            }
+        }
+    }();
+    [arr, col, corner_points, row]()
+    {
+        for (int row_cur{row - 1}; row_cur >= 0; --row_cur)
+        {
+            for (int col_cur{col - 1}; col_cur >= 0; --col_cur)
+            {
+                if (arr[row_cur][col_cur])
+                {
+                    corner_points[2] = row_cur;
+                    corner_points[3] = col_cur;
+                    return;
+                }
+            }
+        }
+    }();
     /***************End your code here***************/
 }
