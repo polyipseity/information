7897ed8d7eeb7f94113591ee519aff269479d89a
William So <polyipseity@gmail.com>
2023-10-12 00:35:23 +0800

    Update ISDN 0003 project to enable USART3
    Original commit: 7a11d141543c49d054a5e7543178749659cc44e0
    
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/academia/engineering/HKUST ISDN 0003/project/Core/Inc/stm32f1xx_it.h
+++ self/academia/engineering/HKUST ISDN 0003/project/Core/Inc/stm32f1xx_it.h
@@ -58,6 +58,7 @@ void SysTick_Handler(void);
 void DMA1_Channel5_IRQHandler(void);
 void TIM3_IRQHandler(void);
 void USART1_IRQHandler(void);
+void USART3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
