d05f7318a0f05f2ae0889c31ae29e5a56a8577da
William So <polyipseity@gmail.com>
2023-11-02 17:52:32 +0800

    Complete COMP 2011 lab 6
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 546a41c63a039a8c75cda3374617418d91fff85e

--- self/academia/computer science/HKUST COMP 2011/lab 6/lab6.cpp
+++ self/academia/computer science/HKUST COMP 2011/lab 6/lab6.cpp
@@ -11,21 +11,40 @@ using namespace std;
 
 const int MAX_LEN = 256; 
 
-/* ADD THE RETURN TYPE HERE */ correct_word(/* ADD FORMAL PARAMETER LIST HERE */)
+bool correct_word(char const string[], int begin, int end)
 {
     
+
    /* ADD YOUR CODE HERE */
    /* THIS MUST BE A RECURSIVE FUNCTION */
-
+   if (begin < 0 || begin > end)
+      return false;
     // Task 1: Base case with only a single character
-
+   if (begin == end && 'a' <= string[begin] && string[begin] <= 'z')
+      return true;
     // Task 2: Recursive case with suffix characters '@', or '#'
-
+   if (string[end] == '@' || string[end] == '#')
+      return correct_word(string, begin, end - 1);
     // Task 3: Recursive case with concatenation characters '|' at suffix position.
-
+   if (string[end] == '|')
+   {
+      for (int ii{begin}; ii <= end - 2; ++ii)
+      {
+         if (correct_word(string, begin, ii) && correct_word(string, ii + 1, end - 1))
+            return true;
+      }
+   }
     // Task 4: Recursive case with concatenation characters '#' at prefix position.
-
+   if (string[begin] == '#')
+   {
+      for (int ii{begin + 1}; ii <= end - 1; ++ii)
+      {
+         if (correct_word(string, begin + 1, ii) && correct_word(string, ii + 1, end))
+            return true;
+      }
+   }
     // All remaining cases are wrong
+   return false;
 }
 
 
