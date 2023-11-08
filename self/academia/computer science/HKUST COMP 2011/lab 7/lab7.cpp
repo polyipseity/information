363df474fec068774ad28e28610d1b04e41a25d4
William So <polyipseity@gmail.com>
2023-11-08 20:58:36 +0800

    Prepare COMP 2011 lab 7
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: b0fe55f9f6a7f43b00f38dcebcd92b820eb88d31

---
+++ self/academia/computer science/HKUST COMP 2011/lab 7/lab7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "lab7.h"
+using namespace std;
+
+// TODO: Reorder the student
+// Parameters:
+// <queue>: An array of pointer to <Student>s. 
+// <num_stduents>: the number of students currently in the <queue>
+// Task:
+// Use the <yearOfStudy> member of the <Student> struct to reorder the students.
+// Students at a higher year of study should be ordered in front.
+// You can use bubble sort for the reordering. The detail instruction is on Lab7 webpage.
+void reorder_student(Student* queue [], int num_students) {
+
+}
