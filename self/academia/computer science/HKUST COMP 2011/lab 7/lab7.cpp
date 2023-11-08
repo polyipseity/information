5364366ee429b8a5f5c30f894dac17564af4489e
William So <polyipseity@gmail.com>
2023-11-08 21:35:08 +0800

    Complete COMP 2011 lab 7 with slight amendment
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 464bb2bbbcb361e4611f803e9da71cd1cc5387fe

--- self/academia/computer science/HKUST COMP 2011/lab 7/lab7.cpp
+++ self/academia/computer science/HKUST COMP 2011/lab 7/lab7.cpp
@@ -17,15 +17,15 @@ void reorder_student(Student *queue[], int num_students)
     return;
   }
   int const mid{num_students / 2};
-  Student **const queue_mid{&queue[mid]}, **const queue_end{&queue[num_students]};
+  Student **const queue_mid{&queue[mid]}, *const *const queue_end{&queue[num_students]};
   reorder_student(queue, mid);
   reorder_student(queue_mid, num_students - mid);
   Student *const *left_iter{queue}, *const *right_iter{queue_mid}, **tmp{new Student *[num_students]};
-  for (int ii{0}; ii < num_students; ++ii)
+  for (int ii{}; ii < num_students; ++ii)
   {
     tmp[ii] = left_iter == queue_mid || right_iter != queue_end && (*right_iter)->yearOfStudy > (*left_iter)->yearOfStudy ? *right_iter++ : *left_iter++;
   }
-  for (int ii{0}; ii < num_students; ++ii)
+  for (int ii{}; ii < num_students; ++ii)
   {
     queue[ii] = tmp[ii];
   }
