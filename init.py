725d19f9736dd6daa182789156ebb87bb2b4f14f
William So <polyipseity@gmail.com>
2023-11-24 00:54:12 +0800

    Fix `init.py`
    Signed-off-by: William So <polyipseity@gmail.com>

--- init.py
+++ init.py
@@ -47,7 +47,6 @@ from pytextgen.main import parser as _pytextgen_parser  # type: ignore
 _EXCLUDES = (
     ".git",
     ".obsidian",
-    "self/academia/computer science/secondary 3/IndividualProgrammingProject/shaded/dglOpenGL/README.md",
     "tools",
 )
 _UUID = "9a27fc39-496b-4b4c-87a7-03b9e88fc6bc"
