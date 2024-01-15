255ca566546d16110ace865b9bcfb06a74f7ba46
William So <polyipseity@gmail.com>
2024-01-15 19:56:42 +0800

    Add Java exercise for lesson 3
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 5ea1d5b62df4127e2111e9792c3f03cce75e8353

---
+++ special/academia/HKUST/COMP 1029J/exercise for lesson 3/template/DimSum.java
@@ -0,0 +1,51 @@
+/**
+ * This class contains the information of a dim sum dish
+ */
+
+public class DimSum
+{
+    //
+    // Task 1 - create the attributes
+    //
+    // The name of the dim sum
+    // The price of the dim sum
+    // The quantity ordered    
+    
+    // Constructor of the class
+    public DimSum(String name, float price) {
+        //
+        // Task 1 - Initialize the attributes of the dim sum
+        // 
+    }
+    
+    // Return the name of the dim sum
+    public String getName() {
+        //
+        // Task 1 - Return the name of the dim sum
+        //
+        return "DimSum";
+    }
+    
+    // Return the price of the dim sum
+    public float getPrice() {
+        //
+        // Task 1 - Return the price of the dim sum
+        //
+        return 0f;
+    }
+    
+    // Return the quantity ordered
+    public int getQuantity() {
+        //
+        // Task 1 - Return the quantity of the dim sum
+        // 
+        return 0;
+    }
+    
+    // Order this dim sum dish
+    public void order(int quantity) {
+        //
+        // Task 1 - Increase the quantity ordered
+        //
+    }
+}
