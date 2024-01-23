969193761c1a7f3dc6c4345ff75a0990ed5dfbdd
William So <polyipseity@gmail.com>
2024-01-23 14:31:25 +0800

    Add Python exercise for lesson 5
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: b14f9aab751b9a2476a43aa93722e1a2f0cc4588

---
+++ special/academia/HKUST/COMP 1029P/exercise for lesson 5/template/tree.py
@@ -0,0 +1,37 @@
+import turtle
+
+# Super fast!
+turtle.speed(0)
+
+# Turn left for 90 degrees
+# Complete this
+
+def tree(trunkLength, currentDepth, maximumDepth):
+    """ Draw a tree with turtle graphics recursively. """
+
+    # Draw a trunk
+    # Complete this
+
+    if currentDepth < maximumDepth:
+        # Turn left for 'angle' degrees
+        # Complete this
+
+        # Recursively draw a smaller tree
+        # Complete this
+
+        # Turn right for 2 * 'angle' degrees
+        # Complete this
+
+        # Recursively draw another smaller tree
+        # Complete this
+
+        # Turn left for 'angle' degrees, so that the 
+        # turtle faces its original starting direction
+        # Complete this
+
+    # Return to the original starting position
+    # Complete this
+
+# Run the tree function
+# You can change the parameters to test it out
+tree(100, 0, 5)
