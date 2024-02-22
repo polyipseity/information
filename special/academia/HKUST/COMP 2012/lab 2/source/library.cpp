bef3b498aa9eb0986a280123139dfb28f9219c68
William So <polyipseity@gmail.com>
2024-02-22 18:46:55 +0800

    Complete COMP 2012 lab 2
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: cfebe2cca0e30296c98156a36143d831efa314b4

--- special/academia/HKUST/COMP 2012/lab 2/source/library.cpp
+++ special/academia/HKUST/COMP 2012/lab 2/source/library.cpp
@@ -1,14 +1,18 @@
 #include "library.h"
 
-Book::Book(const char* name, const char* author, int publishYear) {
+Book::Book(const char* name, const char* author, int publishYear)
+    : name{strcpy(new char[strlen(name) + 1], name)}, author{strcpy(new char[strlen(author) + 1], author)}, publishYear{publishYear} {
     // TODO 1: Book constructor
 }
 
 Book::~Book() {
     // TODO 2: Book destructor
+    delete[] author;
+    delete[] name;
 }
 
-Bookcase::Bookcase(int capacity) {
+Bookcase::Bookcase(int capacity)
+    : books{new Book*[capacity]}, numBooks{}, capacity{capacity} {
     // TODO 3: Bookcase constructor
     // Initialize books to an array of length 'capacity', and numBooks to 0.
     // Hint: how to initialize constant data member capacity?
@@ -16,18 +20,25 @@ Bookcase::Bookcase(int capacity) {
 
 Bookcase::~Bookcase() {
     // TODO 4: Bookcase destructor
+    for (int idx{}; idx < numBooks; ++idx)
+    {
+        delete books[idx];
+    }
+    delete[] books;
 }
 
 void Bookcase::addBook(const char* name, const char* author, int publishYear) {
-    if (false) { // TODO 5.1: full condition
+    if (numBooks >= capacity) { // TODO 5.1: full condition
         cout << "Bookcase is full, cannot add " << name << "!" << endl;
         return;
     }
 
     // TODO 5.2: add a new Book to the books array at index numBooks, then increment numBooks.
+    books[numBooks++] = new Book{name, author, publishYear};
 }
 
-Library::Library(int sciCapacity, int hmnCapacity, int miscCapacity) {
+Library::Library(int sciCapacity, int hmnCapacity, int miscCapacity)
+    : bookcases{sciCapacity, hmnCapacity, miscCapacity} {
     // TODO 6: Library constructor
     // Hint: Bookcase has no default constructor. How to initialize bookcases?
 }
