b5655bc76a7caaccb06047391102c22d73d3912f
William So <polyipseity@gmail.com>
2024-03-15 01:37:37 +0800

    Complete COMP 2012 assignment 1
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: ae05fc6d5bac46dd32a4b7074ffb19916385712e

--- special/academia/HKUST/COMP 2012/assignment 1/source/Buyer.cpp
+++ special/academia/HKUST/COMP 2012/assignment 1/source/Buyer.cpp
@@ -1,26 +1,26 @@
 #include "Buyer.h"
 
 // Task 1.1
-Buyer::Buyer(int id, const string& name) {
+Buyer::Buyer(int id, const string& name) : buyerID{id}, buyerName{name} {
 
 }
 
 // Task 1.2
 void Buyer::setBuyerId(int newId) { 
-
+  buyerID = newId;
 }
 
 // Task 1.3
 int Buyer::getBuyerId() const {
-
+  return buyerID;
 }
 
 // Task 1.4
 void Buyer::setBuyerName(const string& newName) { 
- 
+  buyerName = newName;
 }
 
 // Task 1.5
 string Buyer::getBuyerName() const { 
-
+  return buyerName;
 }
--- special/academia/HKUST/COMP 2012/assignment 1/source/OrderList.cpp
+++ special/academia/HKUST/COMP 2012/assignment 1/source/OrderList.cpp
@@ -4,28 +4,58 @@
 #include <new>       // For bad_alloc
 
 // Task 5.1
-OrderList::OrderList(){
+OrderList::OrderList() : head{} {
 
 }
 
 // Task 5.2
 OrderList::~OrderList() {
-
+    for (OrderNode *iter{head}; iter;)
+    {
+        OrderNode *next{iter->next};
+        delete iter;
+        iter = next;
+    }
 }
 
 // Task 5.3
 void OrderList::addOrder(const Order& order) {
-
+    if (head)
+    {
+        OrderNode *tail{head};
+        for (; tail->next; tail = tail->next)
+            ;
+        tail->next = new OrderNode{order};
+        return;
+    }
+    head = new OrderNode{order};
 }
 
 // Task 5.4
 bool OrderList::removeOrder(int orderId) {
-
+    for (OrderNode *prev{}, *iter{head}; iter; prev = iter, iter = iter->next)
+    {
+        if (iter->order.getOrderId() == orderId)
+        {
+            if (prev)
+                prev->next = iter->next;
+            else
+                head = iter->next;
+            delete iter;
+            return true;
+        }
+    }
+    return false;
 }
 
 // Task 5.5
 Order* OrderList::findOrder(int orderId) const {
-
+    for (OrderNode *iter{head}; iter; iter = iter->next)
+    {
+        if (iter->order.getOrderId() == orderId)
+            return &iter->order;
+    }
+    return nullptr;
 }
 
 // Task 5.6
@@ -43,13 +73,23 @@ void OrderList::displayOrderList() const {
     }
 
 // ------------Write your code here to complete this task-----------------
+    for (OrderNode *iter{head}; iter; iter = iter->next)
+        iter->order.displayOrder();
 }
 
 // Task 5.8
 void OrderList::displayOrdersForBuyer(int buyerId) const {
 
 // ------------Write your code here to complete this task-----------------
-
+    bool found{};
+    for (OrderNode *iter{head}; iter; iter = iter->next)
+    {
+        if (iter->buyer.getBuyerId() == buyerId)
+        {
+            found = true;
+            iter->order.displayOrder();
+        }
+    }
 
 // ---------------------- provided code: DO NOT MODIFY --------------------------
     if (!found) {
