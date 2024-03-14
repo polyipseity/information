9622c3ed9822fb71eb9561c677084bc1699615a3
William So <polyipseity@gmail.com>
2024-03-15 01:48:26 +0800

    Prepare COMP 2012 lab 4
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: 7553f7b66fddac94a210215562bcdc72b789ee82

---
+++ special/academia/HKUST/COMP 2012/lab 4/source/OrdinaryOrder.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+#include "OrdinaryOrder.h"
+
+
+using namespace std;
+
+OrdinaryOrder::OrdinaryOrder(const string& collectionID, const string& name, OrderType type, int capacity) {
+    // TODO 2.1
+    // You can use MIL for TODO 2.1 so you can modify the above part and your code does not neccessarily start from here
+
+    // Your code ends here
+    std::cout << "create " << getCollectionID() << ": OrdinaryOrder constructor" << std::endl;
+}
+
+int OrdinaryOrder::addFromOrder(const OrdinaryOrder* anotherOrder) {
+    // TODO 2.2
+    // Your code starts here
+
+
+
+
+
+
+
+
+    // Your code ends here
+}
