474302360f761db899fd529ed2108ba34eb2fc7d
William So <polyipseity@gmail.com>
2024-04-18 07:05:41 +0800

    Unzip COMP 2012 lab 7
    Signed-off-by: William So <polyipseity@gmail.com>
    Private-commit: bdea074ccbc6b008837b303fb4415118805b895b

---
+++ special/academia/HKUST/COMP 2012/lab 7/source/RandomForest.cpp
@@ -0,0 +1,10 @@
+#include "RandomForest.h"
+
+// TODO 2: 
+// Take as input the test point x.
+// Compute the predictions for every tree in the forest
+// Find and return the majority label
+int RandomForest::ForestPredict( int *x) { 
+    // You can change this
+    return 0;
+}
