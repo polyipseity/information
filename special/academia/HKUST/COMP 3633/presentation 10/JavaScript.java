53636039a4a5b40ee2b1103aea26f4235d76b126
William So <polyipseity@gmail.com>
2024-05-06 16:57:29 +0800

    Add COMP 3633 presentation `JavaScript.java`
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ special/academia/HKUST/COMP 3633/presentation 10/JavaScript.java
@@ -0,0 +1,16 @@
+class JavaScript
+{
+  //\u000A\u002F\u002A
+  function//\u002A\u002Fpublic static void
+  main(
+    //\u000A\u002F\u002A
+    //\u002A\u002FString... args
+  ) {
+    //\u000A\u002F\u002A
+    console.log("I love Java!")//\u002A\u002FSystem.out.println("I love JavaScript!");
+  }
+}
+//\u000A\u002F\u002A
+new JavaScript().main()//\u002A\u002F
+
+// Thanks! https://codegolf.stackexchange.com/a/93311
