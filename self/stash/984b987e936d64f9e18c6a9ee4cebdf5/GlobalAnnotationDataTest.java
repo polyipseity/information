0f4a7ee9a6efe5141c3d21e4fd9f97c91b946206
William So <polyipseity@gmail.com>
2024-07-20 23:22:31 +0800

    Format merged gists
    Signed-off-by: William So <polyipseity@gmail.com>

--- self/stash/984b987e936d64f9e18c6a9ee4cebdf5/GlobalAnnotationDataTest.java
+++ self/stash/984b987e936d64f9e18c6a9ee4cebdf5/GlobalAnnotationDataTest.java
@@ -27,49 +27,38 @@ import java.util.Set;
 @Mod(modid = "GlobalAnnotationDataTest", name = "Global Annotation Data Test", version = "1.0")
 @SuppressWarnings("unused")
 @GlobalAnnotationDataTest.AnnotationTest.TestAnnotation
-public class GlobalAnnotationDataTest
-{
+public class GlobalAnnotationDataTest {
     private static final boolean ENABLE = false;
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        if (ENABLE)
-        {
-            Set<ASMDataTable.ASMData> asmDataSet = event.getAsmData().getAll(AnnotationTest.TestAnnotation.class.getName());
-            String message = "\n--------------------" + AnnotationTest.TestAnnotation.class.getName() + " Debug Message--------------------\n";
-            for (ASMDataTable.ASMData asmData : asmDataSet)
-            {
-                message += "Class Name : " + asmData.getClassName() + "---Object Name : " + asmData.getObjectName() + "\n";
+    public void preInit(FMLPreInitializationEvent event) {
+        if (ENABLE) {
+            Set<ASMDataTable.ASMData> asmDataSet = event.getAsmData()
+                    .getAll(AnnotationTest.TestAnnotation.class.getName());
+            String message = "\n--------------------" + AnnotationTest.TestAnnotation.class.getName()
+                    + " Debug Message--------------------\n";
+            for (ASMDataTable.ASMData asmData : asmDataSet) {
+                message += "Class Name : " + asmData.getClassName() + "---Object Name : " + asmData.getObjectName()
+                        + "\n";
             }
             System.out.println(message);
         }
     }
 
-    public static class AnnotationTest<A, B, C, D, E, F, G, H, I, J, K, L>
-    {
-        public static final class TypeAnnotationTest<@TestAnnotation TEST_TYPE extends Map<AnnotationTest, AnnotationTest>, TEST2 extends Annotation, @AnnotationTest.TestAnnotation TEST3 extends Annotation>
-        {
+    public static class AnnotationTest<A, B, C, D, E, F, G, H, I, J, K, L> {
+        public static final class TypeAnnotationTest<@TestAnnotation TEST_TYPE extends Map<AnnotationTest, AnnotationTest>, TEST2 extends Annotation, @AnnotationTest.TestAnnotation TEST3 extends Annotation> {
             @TestAnnotation
             public static final Class<@TestAnnotation ? extends AnnotationTest<? extends Annotation, NullPointerException, NullPointerException, NullPointerException, NullPointerException, NullPointerException, NullPointerException, NullPointerException, NullPointerException, NullPointerException, NullPointerException, @TestAnnotation NullPointerException>> fieldTypeTest = null;
 
-            public static Class<
-                    @TestAnnotation ? extends Map<
-                            @TestAnnotation ? extends Annotation,
-                            @TestAnnotation ? extends Annotation>>
-            typeAnnotationTest
-                    (@TestAnnotation Class<
-                    @TestAnnotation ? extends Class<
-                            @TestAnnotation ? extends Annotation>> typeTestParameter)
-            {
+            public static Class<@TestAnnotation ? extends Map<@TestAnnotation ? extends Annotation, @TestAnnotation ? extends Annotation>> typeAnnotationTest(
+                    @TestAnnotation Class<@TestAnnotation ? extends Class<@TestAnnotation ? extends Annotation>> typeTestParameter) {
                 return null;
             }
         }
 
-        public static final class MethodAnnotationTest extends AnnotationTest
-        {
-            public static Class<@TestAnnotation ? extends Annotation> methodAnnotationTest(@TestAnnotation Object object, @TestAnnotation Object object2)
-            {
+        public static final class MethodAnnotationTest extends AnnotationTest {
+            public static Class<@TestAnnotation ? extends Annotation> methodAnnotationTest(
+                    @TestAnnotation Object object, @TestAnnotation Object object2) {
                 @TestAnnotation2("Null")
                 String a = null;
                 @TestAnnotation
@@ -80,17 +69,17 @@ public class GlobalAnnotationDataTest
         }
 
         @Retention(RetentionPolicy.RUNTIME)
-        @Target({ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER, ElementType.TYPE_USE, ElementType.LOCAL_VARIABLE})
-        public @interface TestAnnotation
-        {
+        @Target({ ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER,
+                ElementType.TYPE_USE, ElementType.LOCAL_VARIABLE })
+        public @interface TestAnnotation {
             @TestAnnotation
             String value() default "Null";
         }
 
         @Retention(RetentionPolicy.RUNTIME)
-        @Target({ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER, ElementType.TYPE_USE, ElementType.LOCAL_VARIABLE})
-        public @interface TestAnnotation2
-        {
+        @Target({ ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER,
+                ElementType.TYPE_USE, ElementType.LOCAL_VARIABLE })
+        public @interface TestAnnotation2 {
             @TestAnnotation
             String value();
         }
