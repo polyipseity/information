3786d03c3dec35fdaea57e0231d8e89f4fe6b959
William So <polyipseity@gmail.com>
2024-07-29 14:28:36 +0800

    Improve `convert wiki.py` to handle links and lists
    Signed-off-by: William So <polyipseity@gmail.com>

--- tools/convert wiki.py
+++ tools/convert wiki.py
@@ -1,9 +1,136 @@
-from itertools import chain
-from asyncio import run
+from pathlib import PurePath
+from aiohttp import ClientSession, TCPConnector
+from asyncio import gather, run
+from typing import Callable, Mapping, MutableSequence
 from bs4 import BeautifulSoup, NavigableString, PageElement, Tag
 from bs4.element import PreformattedString
 from jaraco.clipboard import paste_html
 from pyperclip import copy
+from sys import version
+from yarl import URL
+
+NAME = PurePath(__file__).name
+AUTHORS = (
+    {
+        "name": "William So",
+        "email": "polyipseity@gmail.com",
+    },
+)
+VERSION = "∞"
+USER_AGENT = f"{NAME}/{VERSION} ({AUTHORS[0]['email']}) Python/{version}"
+_MAX_CONCURRENT_REQUESTS_PER_HOST = 2
+_NAMES_DO_NOT_FIX = frozenset(
+    {
+        "Latin",
+    }
+)
+
+
+def _fix_name_maybe(name: str) -> str:
+    return (
+        f"{name[:1].lower()}{name[1:]}"
+        if name not in _NAMES_DO_NOT_FIX and (len(name) <= 1 or name[1:].islower())
+        else name
+    )
+
+
+def _markdown_link(page: str, fragment: str) -> str:
+    return f"{page.replace(' ', '%20')}.md{fragment and '#'}{fragment.replace(':', '').replace(' ', '%20')}"
+
+
+def _tag_affixes(name: str) -> tuple[str, str]:
+    return f"<{name}>", f"</{name}>"
+
+
+async def wiki_html_to_plaintext(
+    ele: PageElement,
+    *,
+    session: ClientSession,
+    list_stack: MutableSequence[int] = [],
+) -> str:
+    if not isinstance(ele, Tag):
+        return (
+            ele
+            if isinstance(ele, NavigableString)
+            and not isinstance(ele, PreformattedString)
+            and not ele.startswith("<!--")
+            else ""
+        )
+
+    if "reference" in ele.get_attribute_list("class"):
+        return ""
+
+    process_strings: Callable[[str], str] = lambda strings: strings
+    prefix, suffix = "", ""
+    pop_list_stack = False
+    match ele.name:
+        case "b":
+            prefix, suffix = "__", "__"
+        case "i":
+            prefix, suffix = "_", "_"
+        case "s" | "sub" | "sup" | "u":
+            prefix, suffix = _tag_affixes(ele.name)
+        case "a":
+            if title := str(ele.get("title", "")):
+                href = str(ele.get("href", ""))
+                to_fragment = (
+                    href.split("#", 1)[-1].replace("_", " ") if "#" in href else ""
+                )
+                async with session.get(
+                    URL.build(
+                        scheme="https",
+                        host="en.wikipedia.org",
+                        path="/w/api.php",
+                        query={
+                            "format": "json",
+                            "formatversion": 2,
+                            "action": "query",
+                            "titles": title,
+                            "redirects": "",
+                        },
+                    )
+                ) as req:
+                    redirect: Mapping[str, str] = (
+                        (await req.json()).get("query", {}).get("redirects", ({},))[0]
+                    )
+                    to = redirect.get("to", title)
+                    if not to_fragment:
+                        to_fragment = redirect.get("tofragment", "")
+                prefix, suffix = (
+                    "[",
+                    f"]({_markdown_link(_fix_name_maybe(to), _fix_name_maybe(to_fragment))})",
+                )
+                process_strings = (
+                    lambda strings: strings.replace("\\", "\\\\")
+                    .replace(R"[", R"\[")
+                    .replace(R"]", R"\]")
+                )
+        case "ol":
+            pop_list_stack = True
+            list_stack.append(0)
+        case "ul":
+            pop_list_stack = True
+            list_stack.append(-1)
+        case "li":
+            if list_stack and (item := list_stack[-1] + 1) >= 1:
+                list_stack[-1] = item
+                prefix = f"{item}. "
+            else:
+                prefix = "- "
+        case _:
+            pass
+
+    strings = "".join(
+        await gather(
+            *(
+                wiki_html_to_plaintext(child, session=session, list_stack=list_stack)
+                for child in ele.children
+            )
+        )
+    )
+    if pop_list_stack:
+        list_stack.pop()
+    return strings and f"{prefix}{process_strings(strings)}{suffix}"
 
 
 async def main() -> None:
@@ -13,35 +140,15 @@ async def main() -> None:
 
     html = BeautifulSoup(html_text, "html.parser")
 
-    def html_to_plaintext(ele: PageElement) -> str:
-        if not isinstance(ele, Tag):
-            return (
-                ele
-                if isinstance(ele, NavigableString)
-                and not isinstance(ele, PreformattedString)
-                else ""
-            )
-
-        def tag_affixes(name: str):
-            return f"<{name}>", f"</{name}>"
-
-        match ele.name:
-            case "b":
-                prefix, suffix = "__", "__"
-            case "i":
-                prefix, suffix = "_", "_"
-            case "s" | "sub" | "sup" | "u":
-                prefix, suffix = tag_affixes(ele.name)
-            case _:
-                prefix, suffix = "", ""
-
-        return "".join(
-            chain((prefix,), map(html_to_plaintext, ele.children), (suffix,))
-        )
-
-    output = html_to_plaintext(html).replace(
-        "\xa0", " "
-    )  # replace non-breaking spaces with spaces
+    async with ClientSession(
+        connector=TCPConnector(limit_per_host=_MAX_CONCURRENT_REQUESTS_PER_HOST),
+        headers={
+            "Accept-Encoding": "gzip",
+            "User-Agent": USER_AGENT,
+        },
+    ) as session:
+        output = await wiki_html_to_plaintext(html, session=session)
+    output = output.replace("\xa0", " ")  # replace non-breaking spaces with spaces
 
     print(output)
     copy(output)
