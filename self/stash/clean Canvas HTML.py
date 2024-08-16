ab2bf2d58323bb32a9e83d19aabdb4f8bf7040bd
William So <polyipseity@gmail.com>
2024-08-16 18:05:13 +0800

    Add `clean Canvas HTML.py` to stash
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ self/stash/clean Canvas HTML.py
@@ -0,0 +1,42 @@
+from anyio import Path
+from asyncio import gather, run
+from bs4 import BeautifulSoup
+from glob import iglob
+
+# pip install anyio>=3.6.0 beautifulsoup4>=4.12.0
+
+
+async def _process_HTML(path: Path):
+    text = await path.read_text()
+
+    html_tag = BeautifulSoup(text, "html.parser")
+
+    for head_tag in (head_tags := html_tag.find_all("head")):
+        for title_tag in head_tag.find_all("title"):
+            title_tag.extract()
+        for meta_tag in head_tag.find_all("meta"):
+            meta_tag.extract()
+
+    content_wrapper_tag = html_tag.find("div", {"id": "content-wrapper"})
+
+    text = f"""<html>
+{"\n".join(map(str, head_tags))}
+<body>
+{content_wrapper_tag}
+</body>
+</html>"""
+
+    await path.write_text(text)
+
+
+async def main():
+    await gather(
+        *(
+            _process_HTML(Path(path_str))
+            for path_str in iglob("**/*.html", recursive=True)
+        )
+    )
+
+
+if __name__ == "__main__":
+    run(main())
