895fd14b031e8cac86839d9d7d776205469ecdca
William So <polyipseity@gmail.com>
2025-02-02 17:19:03 +0800

    Convert Canvas assignment pages to YAML using `temp.py`
    Signed-off-by: William So <polyipseity@gmail.com>

---
+++ temp.py
@@ -0,0 +1,32 @@
+from glob import glob
+from path import Path
+from subprocess import run
+from sys import executable
+
+
+def main():
+    files = glob("**/*.html", recursive=True)
+    for file in files:
+        if file.startswith(R"self\CTF") or file.endswith("Zinc.html"):
+            continue
+        file = Path(file)
+        data = run(
+            (executable, "tools/convert Canvas assignment.py"),
+            capture_output=True,
+            text=True,
+            input=f"{file}\r\n",
+        )
+        if data.returncode:
+            print(f"Failed: {file}")
+            continue
+        submission = file.with_name("submission.yml")
+        counter = 0
+        while submission.exists():
+            counter += 1
+            submission = submission.with_name(f"submission ({counter}).yml")
+        submission.write_bytes(data.stderr.encode())
+        file.remove()
+
+
+if __name__ == "__main__":
+    main()
